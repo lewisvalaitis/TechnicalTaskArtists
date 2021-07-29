23e2c64d74a98fd94877b14a3237e6157b721415
Lewis Valaitis <valaitislewis@gmail.com>
2021-07-29 07:47:22 +0100

    Workspace and library files.

---
+++ Pods/Target Support Files/Pods-TechnicalTaskArtists-TechnicalTaskArtistsUITests/Pods-TechnicalTaskArtists-TechnicalTaskArtistsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TechnicalTaskArtists_TechnicalTaskArtistsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_TechnicalTaskArtists_TechnicalTaskArtistsUITests
+@end
